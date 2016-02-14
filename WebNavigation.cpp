8e4faa6bccd238d42d1a42daa3089bf1189b1e17
Hyuk-Jun Kwon <widruv@gmail.com>
2016-02-14 17:33:42 +0900

    Problem C: Web Navigation
    Standard web browsers contain features to move backward and forward among the
    
    pages recently visited. One way to implement these features is to use two
    
    stacks to keep track of the pages that can be reached by moving backward and
    
    forward. In this problem, you are asked to implement this.
    
    The following commands need to be supported:
    
    - BACK: Push the current page on the top of the forward stack. Pop the page
    
      from the top of the backward stack, making it the new current page. If the
    
      backward stack is empty, the command is ignored.
    
    - FORWARD: Push the current page on the top of the backward stack. Pop the page
    
      from the top of the forward stack, making it the new current page. If the
    
      forward stack is empty, the command is ignored.
    
    - VISIT <url>: Push the current page on the top of the backward stack, and make
    
      the URL specified the new current page. The forward stack is emptied.
    
    - QUIT: Quit the browser.
    
    Assume that the browser initially loads the web page at the URL
    
    http://www.acm.org/
    
    Input
    
    Input is a sequence of commands. The command keywords BACK, FORWARD, VISIT, and
    
    QUIT are all in uppercase. URLs have no whitespace and have at most 70
    
    characters. You may assume that no problem instance requires more than 100
    
    elements in each stack at any time. The end of input is indicated by the QUIT
    
    command.
    
    Output
    
    For each command other than QUIT, print the URL of the current page after the
    
    command is executed if the command is not ignored. Otherwise, print "Ignored".
    
    The output for each command should be printed on its own line. No output is
    
    produced for the QUIT command.
    
    Sample Input
    
    VISIT http://acm.ashland.edu/
    
    VISIT http://acm.baylor.edu/acmicpc/
    
    BACK
    
    BACK
    
    BACK
    
    FORWARD
    
    VISIT http://www.ibm.com/
    
    BACK
    
    BACK
    
    FORWARD
    
    FORWARD
    
    FORWARD
    
    QUIT
    
    Sample Output
    
    http://acm.ashland.edu/
    
    http://acm.baylor.edu/acmicpc/
    
    http://acm.ashland.edu/
    
    http://www.acm.org/
    
    Ignored
    
    http://acm.ashland.edu/
    
    http://www.ibm.com/
    
    http://acm.ashland.edu/
    
    http://www.acm.org/
    
    http://acm.ashland.edu/
    
    http://www.ibm.com/
    
    Ignored

---
+++ WebNavigation.cpp
@@ -0,0 +1,109 @@
+#include "WebNavigation.h"
+
+WebNavigation::WebNavigation()
+{
+	_initialPage = "http://www.acm.org/";
+	_currentPage = _initialPage;
+	_failMsg = "Ignored";
+
+	_backCommand = "BACK";
+	_forwardCommand = "FORWARD";
+	_visitCommand = "VISIT";
+	_quitCommand = "QUIT";
+}
+
+bool WebNavigation::excute( std::string command,
+		std::string& currentPage )
+{
+	if( strcmp( command.c_str(), _quitCommand ) == 0 )
+	{
+		return quit();
+	}
+
+	currentPage = _failMsg;
+	if( strcmp( command.c_str(), _backCommand ) == 0 )
+	{
+		currentPage = back();
+	}
+	else if( strcmp( command.c_str(), _forwardCommand ) == 0 )
+	{
+		currentPage = forward();
+	}
+	else if( strncmp( command.c_str(), _visitCommand, 5 ) == 0 )
+	{
+		int startPosition = getWebSiteStartPosition( &command[5] );
+		if( startPosition != -1 )
+		{
+			currentPage = visit( &command[ 5 + startPosition ] );
+		}
+	}
+
+	return SUCCESS;
+}
+
+std::string WebNavigation::back()
+{
+	if( _backwardStack.empty() )
+	{
+		return _failMsg;
+	}
+	_forwardStack.push( _currentPage );
+	_currentPage = _backwardStack.top();
+	_backwardStack.pop();
+
+	return _currentPage;
+}
+
+std::string WebNavigation::forward()
+{
+	if( _forwardStack.empty() )
+	{
+		return _failMsg;
+	}
+
+	_backwardStack.push( _currentPage );
+	_currentPage = _forwardStack.top();
+	_forwardStack.pop();
+
+	return _currentPage;
+}
+
+std::string WebNavigation::visit( const std::string webSite )
+{
+	_backwardStack.push( _currentPage );
+	_currentPage = webSite;
+
+	clearForwardStack();
+
+	return _currentPage;
+}
+
+bool WebNavigation::quit()
+{
+	return QUIT;
+}
+
+int WebNavigation::getWebSiteStartPosition( char* afterVisitCommand )
+{
+	if( afterVisitCommand[0] == '\0' || !isspace(afterVisitCommand[0]) )
+	{
+		return -1;
+	}
+
+	for( int i = 1 ; i < strlen(afterVisitCommand) ; ++i )
+	{
+		if( !isspace(afterVisitCommand[i]) )
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void WebNavigation::clearForwardStack()
+{
+	while( !_forwardStack.empty() )
+	{
+		_forwardStack.pop();
+	}
+}
